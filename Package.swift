be4edd7d9cf8da4fa4c35888b11a20bd47665ed9
Dongcp <d-chu.run@gmofh.com>
2019-08-19 11:01:11 +0900

    swift package

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MJRefresh",
+    products: [
+        .library(
+            name: "MJRefresh",
+            targets: ["MJRefresh"])
+    ],
+    targets: [
+        .target(
+            name: "MJRefresh",
+            path: "MJRefresh")
+    ]
+)
+
